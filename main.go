b3a9939dcf104b0bc8c0534be6d36a1b42e86c53
Oren <ogolan@yp.com>
2014-05-22 21:10:35 -0700

    init

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db := SetupDB()
+	rows, err := db.Query("SELECT title, author, description FROM books")
+
+	PanicIf(err)
+	defer rows.Close()
+
+	var title, author, description string
+	for rows.Next() {
+		err := rows.Scan(&title, &author, &description)
+		PanicIf(err)
+		fmt.Printf("Title: %s\nAuthor: %s\nDescription: %s\n\n", title, author, description)
+	}
+}
+func PanicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// dials the database, returning any error
+func SetupDB() *sql.DB {
+	db, err := sql.Open("postgres", "user=dev password=password dbname=test sslmode=disable")
+	PanicIf(err)
+
+	return db
+}
